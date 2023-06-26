85a250a039d774c626e3d4fa0557dc6f514d5b53
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-26 20:56:01 +0400

    add html and css codes

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
     <title>register-form</title>
 </head>
 <body>
+    <div class="container">
+        <din class="img">
+            <div class="box-form">
+                <h2>Register</h2>
+                <div class="box-input">
+                    <input type="text" name="user-name" id="name" placeholder="John Doe">
+                    <label for="name">User Name</label>
+                </div>
+                <div class="box-input">
+                    <input type="email" name="email" id="email" placeholder="John Doe@gmail.com">
+                    <label for="email">Email</label>
+                </div>
+                <div class="box-input">
+                    <input type="password" name="password" id="pass" placeholder="More Than 6 Char">
+                    <label for="pass">Password</label>
+                </div>
+                <div class="box-input">
+                    <input type="password" name="confirm" id="conf" placeholder="Same Password">
+                    <label for="conf">Re Password</label>
+                </div>
+            </div>
+        </din>
+    </div>
     
 </body>
 </html>
