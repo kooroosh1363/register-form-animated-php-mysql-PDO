c40947b79d96c85cf1ea9c2b9c557aad1d9c848c
kooroosh1363 <peymanraad1363@gmail.com>
2023-06-28 16:09:04 +0400

    chang video background and background color

--- register.php
+++ register.php
@@ -59,7 +59,7 @@ if (
 
 <body>
     <video autoplay muted loop plays-inline class="video">
-        <source src="./assets/video/rr-(1080p).mp4" type="video/mp4">
+        <source src="./assets/video/ww(2160p).mp4" type="video/mp4">
     </video>
     <div class="container">
 
@@ -84,7 +84,7 @@ if (
                         <label for="conf">RePassword</label>
                     </div>
                     <div class="group">
-                        <a href="#" style="color:#0c1950;">Forget Password?</a>
+                        <a href="#" style="color:#cc979e;">Forget Password?</a>
                         <a href="./login.php">Login</a>
                     </div>
                     <button name="sub" class="btn">Sign In</button>
